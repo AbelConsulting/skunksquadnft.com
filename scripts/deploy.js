5d0ee028ab0026c01d327dcc825b9dbd262da7fa
AbelConsulting <marcfabel@gmail.com>
2025-10-10 18:34:46 -0600

    tinker
    tinker

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -1,10 +1,6 @@
-import { ethers } from "hardhat";
-import fs from 'fs';
-import path from 'path';
-import { fileURLToPath } from 'url';
-
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
+const { ethers } = require("hardhat");
+const fs = require('fs');
+const path = require('path');
 
 async function main() {
     console.log("🦨 Starting SkunkSquad NFT Ultra-Smart Contract Deployment...\n");
